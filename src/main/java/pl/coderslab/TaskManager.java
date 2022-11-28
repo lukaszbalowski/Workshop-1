837178268e04aa93d3ad53dc36ceaf58c0ae540f
lukaszbalowski <l.balowski@gmail.com>
2022-11-28 12:46:27 +0100

    fileread method

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,73 +1,76 @@
 package pl.coderslab;
 
 
-import java.io.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class TaskManager {
 
+    static final String fileName = "tasks.csv";
+    static final String[] options = {"add", "remove", "list", "exit"};
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
 
-    try {
-        String[] options = {"Please select an option:", "add", "remove", "list", "exit"};
-        String[][] tasks = {};
-    for (String menu: options) {
-        System.out.println(menu);
-    };
+        String[][] tasks;
+        showOptions(options);
 
-    Scanner scan = new Scanner (System.in);
-    System.out.println("Please select your choice, and press enter: ");
+        String input = scan.nextLine();
+        if (input.equals("exit")) {
+            System.out.println("Do zobaczenia!");
+            System.exit(0);
+        } else {
+            System.out.println("Jeszcze nie gotowe. Spróbuj później");
+            System.exit(0);
+        }
 
-    String input = scan.nextLine();
-    scan.close();
-
-    if ("list".equals(input)) {
-
-        lineCount();
+    }
 
 
-/*        Path path = Paths.get("tasks.csv");
+    public static void showOptions(String[] arr) {
+        System.out.println(ConsoleColors.BLUE);
+        System.out.println("Please select an option, and press 'enter' :  " + ConsoleColors.RESET);
+        for (String option : arr) {
+            System.out.println(option);
 
+        }
+
+
+    }
+
+    public static String[][] distributeDataToArray (String filename) throws IOException {
+
+        Path fileLocation = Paths.get(fileName);
+        if (!Files.exists(fileLocation)) {
+            System.out.println("Plik nie istnieje");
+            System.exit(0);
+        }
+
+        String[][] tab = {};
 
         try {
-            lineCount = (int) Files.lines(path).count();
+            List<String> strings = Files.readAllLines(fileLocation);
+            tab = new String[strings.size()][strings.get(0).split(",").length];
+
+            for (int i = 0; i < strings.size(); i++) {
+                String[] split = strings.get(i).split(",");
+                for (int j = 0; j < split.length; j++) {
+                    tab[i][j] = split[j];
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(lineCount); // todo  ogarnąć wyjątki przy tej części*/
-
-    }
-    } catch (FileNotFoundException e) { // todo rozwiązać problem z wyjątkiem FileNotFoundException
-        System.out.println("Błąd pliku" + e);
-    }
-
-    }
-
-    public static int lineCount() { // todo metoda do zlicznia lini w tekście?
-        int lineCount = 0;
-        try {
-            File file = new File ("tasks.csv");
-            Scanner scan = new Scanner (file);
-
-            while (scan.hasNextLine()) {
-                scan.nextLine();
-                lineCount++;
-            }
-            System.out.println(lineCount);
-            scan.close();
-
-        } catch (Exception e) {
-            e.getStackTrace();
-        }
-        return lineCount;
-
+        return tab;
 
 
     }
+
+
+
 }
