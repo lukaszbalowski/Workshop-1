bce45206f92039dcd03bc7e698da1a0d96873b1c
lukaszbalowski <l.balowski@gmail.com>
2022-11-28 01:11:58 +0100

    lineCount method

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -25,23 +25,49 @@ public class TaskManager {
     System.out.println("Please select your choice, and press enter: ");
 
     String input = scan.nextLine();
+    scan.close();
 
     if ("list".equals(input)) {
 
-        Path path = Paths.get("tasks.csv");
-        int lineCount = 0;
+        lineCount();
+
+
+/*        Path path = Paths.get("tasks.csv");
+
 
         try {
             lineCount = (int) Files.lines(path).count();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(lineCount); // todo  ogarnąć wyjątki przy tej części
+        System.out.println(lineCount); // todo  ogarnąć wyjątki przy tej części*/
 
     }
-    } catch (FileNotFoundException e) {
+    } catch (FileNotFoundException e) { // todo rozwiązać problem z wyjątkiem FileNotFoundException
         System.out.println("Błąd pliku" + e);
     }
+
+    }
+
+    public static int lineCount() { // todo metoda do zlicznia lini w tekście?
+        int lineCount = 0;
+        try {
+            File file = new File ("tasks.csv");
+            Scanner scan = new Scanner (file);
+
+            while (scan.hasNextLine()) {
+                scan.nextLine();
+                lineCount++;
+            }
+            System.out.println(lineCount);
+            scan.close();
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return lineCount;
+
+
 
     }
 }
